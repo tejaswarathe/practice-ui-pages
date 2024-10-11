d92235b448e5e87977a9bbc4eac3ee5426a37345
tejaswarathe <tejaswarathe@gmail.com>
2024-10-11 23:34:01 +0530

    Bento grid initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var a = 100;
+let b = 200;
+const c = 300;
+if (true) {
+  //   var b = 10;
+  let c = 1000000;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "DM-Sans": ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        lightPurple: "#dacffc",
+        darkPurple: "#7650dc",
+        ligthYellow: "#f9eee1",
+        darkYellow: "#ffcb6b",
+        white: "#ffffff",
+        black: "#121212",
+        grey: "#efefef",
+      },
+    },
   },
   plugins: [],
 };
